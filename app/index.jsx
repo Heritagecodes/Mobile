a83b4f5ae5e11084d89ccab34065ad75a03d5986
Heritagecodes <adesokanheritage@gmail.com>
2026-01-27 10:51:08 -0800

    Week 3

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+
+
+import { Text, View } from 'react-native';
+
+export default function index() {
+  
+  return (
+    <View style={{
+      backgroundColor: "black",
+      flex:0.9
+    }}>
+      
+      <Text style={{fontSize:50, color: "green", fontWeight: '900', textAlign: 'center'}}
+      >Hello World!</Text>
+      <Text>My Name is Heritage</Text>
+      <Text>I love coding</Text>
+    </View>
+
+  )
+}
